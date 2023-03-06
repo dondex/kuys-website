695a51e2af4363412a25998aa704dd8151808dda
Dexter <dex.chavez@yahoo.com>
2023-03-07 04:40:01 +0800

    cdn top

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,8 +5,9 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>Kuy's Website</title>
-        @vite("resources/js/app.js")
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+        @vite("resources/js/app.js")
+
 
     </head>
     <body>
